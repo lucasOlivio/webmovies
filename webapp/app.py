f0422acb4d277e4a4b0978cc045617eba874e42f
lucasOlivio <lucas27_olivio@hotmail.com>
2019-08-16 17:18:43 -0300

    Página de login inicial

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from forms import Login
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234567890'
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    form = Login()
+    if form.validate_on_submit():
+        print('OK')
+
+        return redirect(url_for('index'))
+    return render_template('login.html',form=form)
 
 if __name__ == "__main__":
     app.run()
